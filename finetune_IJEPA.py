721456a3c3e19186c8e0eb99794c7dbfeca0a877
ravioliasb <ravioliasb@gmail.com>
2026-04-14 22:05:16 -0600

    Added IJEPA Files

---
+++ finetune_IJEPA.py
@@ -0,0 +1,187 @@
+import numpy as np
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, ModelSummary
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.tensorboard import SummaryWriter
+from model import IJEPA_base
+from pretrain_IJEPA import IJEPA
+import os
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+class IJEPADataset(Dataset):
+    def __init__(self, dataset_path, stage='train', transform=None):
+        super().__init__()
+        if stage == 'train':
+            self.transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(15),               
+                transforms.Grayscale(num_output_channels=3),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.225, 0.225, 0.225])
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.Grayscale(num_output_channels=3),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.225, 0.225, 0.225])
+            ])
+        self.data = datasets.ImageFolder(os.path.join(dataset_path, stage), transform=self.transform)
+        self.num_classes = len(self.data.classes)
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        img, label = self.data[index]
+        one_hot_label = np.zeros(self.num_classes)
+        one_hot_label[label] = 1
+        return img, one_hot_label
+
+class D2VDataModule(pl.LightningDataModule):
+    def __init__(self, dataset_path, batch_size=128, num_workers=4, pin_memory=True, shuffle=True):
+        super().__init__()
+        self.dataset_path = dataset_path
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.shuffle = shuffle
+
+    def setup(self, stage=None):
+        self.train_dataset = IJEPADataset(dataset_path=self.dataset_path, stage='train')
+        self.val_dataset = IJEPADataset(dataset_path=self.dataset_path, stage='val')
+        self.test_dataset = IJEPADataset(dataset_path=self.dataset_path, stage='test')
+        
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=self.shuffle,
+        )
+    
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=False,
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=False,
+        )
+
+class IJEPA_FT(pl.LightningModule):
+    def __init__(self, pretrained_model_path, num_classes, lr=1e-3, weight_decay=0, drop_path=0.1):
+        super().__init__()
+        self.save_hyperparameters()
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.drop_path = drop_path
+
+        self.pretrained_model = IJEPA.load_from_checkpoint(pretrained_model_path)
+        self.pretrained_model.model.mode = "test"
+        self.pretrained_model.model.layer_dropout = self.drop_path
+        self.average_pool = nn.AvgPool1d((self.pretrained_model.embed_dim), stride=1)
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(self.pretrained_model.num_tokens),
+            nn.Linear(self.pretrained_model.num_tokens, num_classes),
+        )
+        self.criterion = nn.CrossEntropyLoss()
+        self.writer = SummaryWriter()
+
+    def forward(self, x):
+        x = self.pretrained_model.model(x)
+        x = self.average_pool(x)
+        x = x.squeeze(-1)
+        x = self.mlp_head(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch   
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        accuracy = (y_hat.argmax(dim=1) == y.argmax(dim=1)).float().mean()
+        self.log('train_accuracy', accuracy, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.writer.add_scalar("Train/accuracy", accuracy, self.global_step)
+        self.writer.add_scalar("Train/loss", loss, self.global_step)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch  
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        accuracy = (y_hat.argmax(dim=1) == y.argmax(dim=1)).float().mean()
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_accuracy', accuracy, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.writer.add_scalar("Val/accuracy", accuracy, self.global_step)
+        self.writer.add_scalar("Val/loss", loss, self.global_step)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        accuracy = (y_hat.argmax(dim=1) == y.argmax(dim=1)).float().mean()
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('test_accuracy', accuracy, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+    
+    def predict_step(self, batch, batch_idx, dataloader_idx):
+        return self(batch[1])
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        return optimizer
+
+if __name__ == '__main__':
+    dataset = D2VDataModule(dataset_path='/projects/earlydetection/Sagittal')
+    model = IJEPA_FT(pretrained_model_path='pretrain-checkpoints-Med/last.ckpt', num_classes=11)
+
+    logger = TensorBoardLogger("lightning_logs", name="IJEPA_finetune_logs")
+    lr_monitor = LearningRateMonitor(logging_interval="step")
+    model_summary = ModelSummary(max_depth=2)
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_accuracy',
+        dirpath='finetune-checkpoints-Med/',
+        filename='{epoch:02d}-{val_accuracy:.2f}',
+        save_top_k=1,
+        mode='max',
+        save_last=True
+    )
+
+    trainer = pl.Trainer(
+        accelerator='gpu',
+        devices=1,
+        precision="bf16-mixed",
+        max_epochs=40,
+        callbacks=[lr_monitor, model_summary, checkpoint_callback],
+        gradient_clip_val=.1,
+        logger=logger, 
+    )
+
+    trainer.fit(model, dataset)
+    #trainer.test(model, datamodule=dataset)
+
+    best_checkpoint_path = checkpoint_callback.best_model_path
+    model = IJEPA_FT.load_from_checkpoint(best_checkpoint_path, num_classes=11)
+    
+    # Test the model with the best checkpoint
+    trainer.test(model, datamodule=dataset)
---
+++ pretrain_IJEPA.py
@@ -0,0 +1,228 @@
+import numpy as np
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Subset
+from pytorch_lightning.callbacks import (
+    ModelCheckpoint,
+    LearningRateMonitor,
+    ModelSummary,
+)
+from pytorch_lightning.loggers import WandbLogger
+from model import IJEPA_base
+import os
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+class IJEPADataset(Dataset):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+
+class D2VDataModule(pl.LightningDataModule):
+    def __init__(self, dataset_path, batch_size=16, num_workers=4, pin_memory=True, shuffle=True):
+        super().__init__()
+        self.dataset_path = dataset_path
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.shuffle = shuffle
+        self.transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor(),
+            transforms.Resize((224, 224)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+
+    def setup(self, stage=None):
+        self.train_dataset = datasets.ImageFolder(os.path.join(self.dataset_path, 'train'), transform=self.transform)
+
+        #train_size = len(self.train_dataset)
+        #subset_size = train_size // 200
+        #indices = np.random.choice(train_size, subset_size, replace=False)
+        #self.train_dataset = Subset(self.train_dataset, indices)
+
+        self.val_dataset = datasets.ImageFolder(os.path.join(self.dataset_path, 'val'), transform=self.transform)
+        
+        #val_size = len(self.val_dataset)
+        #subset_size = val_size // 200
+        #indices = np.random.choice(val_size, subset_size, replace=False)
+        #self.val_dataset = Subset(self.val_dataset, indices)
+
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=self.shuffle)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
+
+
+class IJEPA(pl.LightningModule):
+    def __init__(
+            self,
+            img_size=224,
+            patch_size=16,
+            in_chans=3, 
+            embed_dim=64,
+            enc_heads=8,
+            enc_depth=8,
+            decoder_depth=6,
+            lr=1e-6,
+            weight_decay=0.05,
+            target_aspect_ratio = (0.75,1.5),
+            target_scale = (0.15, .2),
+            context_aspect_ratio = 1,
+            context_scale = (0.85,1.0),
+            M = 4, #number of different target blocks
+            m=0.996, #momentum
+            m_start_end = (.996, 1.)
+
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        
+        #define models
+        self.model = IJEPA_base(img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim, 
+                                enc_depth = enc_depth, num_heads=enc_heads, pred_depth=decoder_depth, M=M)
+
+        #define hyperparameters
+        self.M = M
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.m = m
+        self.target_aspect_ratio = target_aspect_ratio
+        self.target_scale = target_scale
+        self.context_aspect_ratio = context_aspect_ratio
+        self.context_scale = context_scale
+        self.embed_dim = embed_dim
+        self.patch_size = patch_size
+        self.num_tokens = (img_size // patch_size) ** 2
+        self.m_start_end = m_start_end
+
+        #define loss
+        self.criterion = nn.MSELoss()
+
+        self.writer = SummaryWriter()
+
+    
+    def forward(self, x, target_aspect_ratio, target_scale, context_aspect_ratio, context_scale):
+        return self.model(x, target_aspect_ratio, target_scale, context_aspect_ratio, context_scale)
+    
+    '''Update momentum for teacher encoder'''
+    def update_momentum(self, m):
+        student_model = self.model.student_encoder.eval()
+        teacher_model = self.model.teacher_encoder.eval()
+        with torch.no_grad():
+            for student_param, teacher_param in zip(student_model.parameters(), teacher_model.parameters()):
+                teacher_param.data.mul_(other=m).add_(other=student_param.data, alpha=1 - m)
+
+
+    def training_step(self, batch, batch_idx):
+        x = batch[0]
+        #generate random target and context aspect ratio and scale
+        target_aspect_ratio = np.random.uniform(self.target_aspect_ratio[0], self.target_aspect_ratio[1])
+        target_scale = np.random.uniform(self.target_scale[0], self.target_scale[1])
+        context_aspect_ratio = self.context_aspect_ratio
+        context_scale = np.random.uniform(self.context_scale[0], self.context_scale[1])
+
+        y_student, y_teacher = self(x, target_aspect_ratio, target_scale, context_aspect_ratio, context_scale)
+        loss = self.criterion(y_student, y_teacher)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.writer.add_scalar("Train/loss", loss, self.global_step)
+                    
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x = batch[0]
+        target_aspect_ratio = np.random.uniform(self.target_aspect_ratio[0], self.target_aspect_ratio[1])
+        target_scale = np.random.uniform(self.target_scale[0], self.target_scale[1])
+        context_aspect_ratio = self.context_aspect_ratio
+        context_scale = np.random.uniform(self.context_scale[0], self.context_scale[1])
+
+        y_student, y_teacher = self(x, target_aspect_ratio, target_scale, context_aspect_ratio, context_scale)
+        loss = self.criterion(y_student, y_teacher)
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.writer.add_scalar("Val/loss", loss, self.global_step)
+
+        
+        return loss
+    
+    def predict_step(self, batch, batch_idx, dataloader_idx):
+        target_aspect_ratio = np.random.uniform(self.target_aspect_ratio[0], self.target_aspect_ratio[1])
+        target_scale = np.random.uniform(self.target_scale[0], self.target_scale[1])
+        context_aspect_ratio = self.context_aspect_ratio
+        context_scale = 1
+        self.model.mode = "test"
+
+        return self(batch, target_aspect_ratio, target_scale, context_aspect_ratio, context_scale) #just get teacher embedding
+
+    def on_after_backward(self):
+        self.update_momentum(self.m)
+        self.m += (self.m_start_end[1] - self.m_start_end[0]) / self.trainer.estimated_stepping_batches
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer,
+            max_lr=self.lr,
+            total_steps=self.trainer.estimated_stepping_batches,
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "step",
+            },
+        }
+
+
+if __name__ == '__main__':
+    dataset = D2VDataModule(dataset_path='/projects/earlydetection/imagenet')
+
+
+    model = IJEPA(img_size=224, patch_size=16, in_chans=3, embed_dim=64, enc_heads=8, enc_depth=8, decoder_depth=6, lr=1e-3)
+
+    logger = TensorBoardLogger("lightning_logs", name="IJEPA_pretrain_logs")
+
+
+    lr_monitor = LearningRateMonitor(logging_interval="step")
+    model_summary = ModelSummary(max_depth=2)
+
+        
+    checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',  
+    dirpath='pretrain-checkpoints/',  
+    filename='{epoch:02d}-{val_loss:.5f}',  
+    save_top_k=3,  
+    mode='min',
+    save_last=True  
+    )
+
+    #accumulate_steps = 16
+
+    trainer = pl.Trainer(
+        accelerator='gpu',
+        devices=1,
+        precision="bf16-mixed",
+        max_epochs=100,
+        callbacks=[lr_monitor, model_summary, checkpoint_callback],
+        gradient_clip_val=.1,
+        #gradient_clip_algorithm="value",
+        logger=logger,
+        #accumulate_grad_batches=accumulate_steps,
+    )
+
+    trainer.fit(model, dataset)
